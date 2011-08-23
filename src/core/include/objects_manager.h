75e8c99e2a40cbf908d0338b53d218a07af2551c
Serge Ratke <serge.ratke@gmx.de>
2011-08-23 15:08:50 +0200

    getObject templified.
    made getObject a template method.

--- src/core/include/objects_manager.h
+++ src/core/include/objects_manager.h
@@ -10,7 +10,16 @@ namespace Baukasten {
 		virtual ~ObjectsManager();
 
 		void addObject( void*, const std::string& );
-		void* getObject( const std::string& ) const;
+
+		template<class T>
+		T getObject( const std::string &id ) const
+		{
+			ObjectMap::const_iterator it = mObjects.find( id );
+			return ( it == mObjects.end() )
+				? 0
+				: static_cast<T>( it->second );
+		}
+
 		bool hasObject( const std::string& ) const;
 		void removeObject( const std::string& );
 
--- src/core/src/objects_manager.cpp
+++ src/core/src/objects_manager.cpp
@@ -16,12 +16,6 @@ void ObjectsManager::addObject( void *object, const std::string &id )
 		mObjects[ id ] = object;
 }
 
-void* ObjectsManager::getObject( const std::string &id ) const
-{
-	ObjectMap::const_iterator it = mObjects.find( id );
-	return ( it == mObjects.end() ) ? 0 : it->second;
-}
-
 bool ObjectsManager::hasObject( const std::string &id ) const
 {
 	return ( mObjects.find( id ) != mObjects.end() );
