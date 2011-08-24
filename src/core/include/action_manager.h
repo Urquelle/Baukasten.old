9e0a4ceaadf9a268af575f413c338d154c240669
Serge Ratke <serge.ratke@gmx.de>
2011-08-24 15:57:18 +0200

    actionmanager documented.

--- src/core/include/action_manager.h
+++ src/core/include/action_manager.h
@@ -7,23 +7,104 @@
 namespace Baukasten {
 	class Action;
 
+	/*! \brief implements action container class.
+	 *
+	 * \ingroup action
+	 *
+	 * wrapps methods implemented in AbstractManager to limit the usage
+	 * of this class to Action classes only.
+	 */
 	class BAUKASTEN_EXPORT ActionManager : public AbstractManager<Action> {
 	public:
+
+		/*! \brief constructor */
 		ActionManager();
+
+		/*! \brief destructor */
 		virtual ~ActionManager();
 
+		/*! \brief adds an Action object to the map.
+		 *
+		 * adds the given Action object to the map. Action::getId() is
+		 * used as the key for the map.
+		 *
+		 * \param action Action object to insert.
+		 * \sa addAction( const std::string&, Action* )
+		 */
 		void addAction( Action* );
+
+		/*! \brief adds an Action object to the map.
+		 *
+		 * adds the given Action object to the map. the given id
+		 * is used as the key for the map.
+		 *
+		 * \param action Action object to insert.
+		 * \param id string which is used as the map key.
+		 */
 		void addAction( const std::string&, Action* );
+
+		/*! \brief returns the Action object to the given id.
+		 *
+		 * returns the Action object to the given id, if the key
+		 * is not in the map a NULL is returned.
+		 *
+		 * \param id key in the map under which the object was inserted.
+		 * \return a pointer to the Action object if found, NULL otherwise.
+		 */
 		Action* getAction( const std::string& ) const;
+
+		/*! \brief returns the map of objects. */
 		ActionMap getActions() const;
+
+		/*! \brief returns wheter the the key's present in the map.
+		 *
+		 * \return true if key found, false otherwise.
+		 */
 		bool hasAction( const std::string& ) const;
 
+		/*! \brief put action into the execution queue.
+		 *
+		 * puts the action with the given id into the execution
+		 * queue, with source object as its target.
+		 *
+		 * \param id id of the action.
+		 */
 		void invokeAction( const std::string& );
+
+		/*! \brief put action into the execution queue.
+		 *
+		 * puts the action with the given id into the execution
+		 * queue, with the given GameEntity object as its target.
+		 *
+		 * \param id id of the action.
+		 * \param target target object.
+		 */
 		void invokeAction( const std::string&, GameEntity* );
+
+		/*! \brief put action into the execution queue.
+		 *
+		 * puts the action with the given id into the execution
+		 * queue, with the given GameEntityList as its targets.
+		 *
+		 * \param id id of the action.
+		 * \param targets target objects.
+		 */
 		void invokeAction( const std::string&, GameEntityList );
 
+		/*! \brief drop action from the execution queue.
+		 *
+		 * removes the action with the given id from the execution
+		 * queue.
+		 *
+		 * \param id id of action.
+		 */
 		void dropAction( const std::string& );
 
+		/*! \brief execute actions in the execution queue.
+		 *
+		 * calls the run() method of every action in the execution
+		 * queue.
+		 */
 		void runActions();
 
 	private:
