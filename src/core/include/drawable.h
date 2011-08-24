c16d9cff82a029f00c362635d0cedd9a278199cd
Serge Ratke <serge.ratke@gmx.de>
2011-08-24 16:37:46 +0200

    commented drawable.

--- src/core/include/drawable.h
+++ src/core/include/drawable.h
@@ -5,24 +5,61 @@
 #include "entity.h"
 
 namespace Baukasten {
+
+	/*! \brief represents a drawable object.
+	 *
+	 * \ingroup entity
+	 *
+	 * drawable objects can be stored in the VirtualSpace
+	 * map and be rendered by whatever rendering engine is used.
+	 */
 	class BAUKASTEN_EXPORT Drawable : public Entity {
 	public:
+
+		/*! \brief constructor
+		 *
+		 * \param id object id.
+		 */
 		Drawable( const std::string& );
+
+		/*! \brief destructor */
 		virtual ~Drawable();
 
+		/*! \brief set position of the object.
+		 *
+		 * \param position a three point vector which represents
+		 * the position of the object in a 3d space.
+		 */
 		void setPosition( const t_pos );
+
+		/*! \brief getter method for position. */
 		const t_pos getPosition() const;
 
+		/*! \brief set the rotation around the x-axis.
+		 *
+		 * \param pitch rotation value.
+		 */
 		void setPitch( const float );
+
+		/*! getter method for the rotation around the x-axis. */
 		const float getPitch() const;
 
+		/*! setter method for object's rotation around the y-axis. */
 		void setYaw( const float );
+
+		/*! getter method for object's rotation around the y-axis. */
 		const float getYaw() const;
 
+		/*! setter method for object's rotation around the z-axis. */
 		void setRoll( const float );
+
+		/*! getter method for object's rotation around the z-axis. */
 		const float getRoll() const;
 
+		/*! setter method for object's scale ratio. */
 		void setScale( const float );
+
+		/*! getter method for object's scale ratio. */
 		const float getScale() const;
 
 	protected:
