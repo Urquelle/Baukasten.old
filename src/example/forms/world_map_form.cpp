5e29ecf04c4028d4653d364a1b50288d25b28675
Serge Ratke <serge.ratke@gmx.de>
2011-08-25 21:00:58 +0200

    partially implemented the scene rendering.
    worldmap renders itself now. all subsequent objects -- if present --
    will be created in the worldmap entity and added to its form object to
    be rendered.

--- src/example/forms/world_map_form.cpp
+++ src/example/forms/world_map_form.cpp
@@ -5,6 +5,32 @@
 WorldMapForm::WorldMapForm( const std::string &id, Ogre::Root *root ) :
 	OgreForm( id, root )
 {
+	/**
+	 * initialise the scene.
+	 *
+	 * position the camera and add a viewport. create the entities
+	 * that belong to the worldmap itself.
+	 */
+
+	Ogre::SceneManager *sceneManager = root->getSceneManager( "sceneManager" );
+
+	// create camera
+	Ogre::Camera *mCamera = sceneManager->createCamera("Camera");
+
+    mCamera->setPosition( Ogre::Vector3( 0, 400, 500) );
+    mCamera->lookAt(Ogre::Vector3(0,0,0));
+    mCamera->setNearClipDistance(5);
+
+	// add viewport
+	Ogre::RenderWindow *window = root->getAutoCreatedWindow();
+    Ogre::Viewport *viewport = window->addViewport(mCamera);
+    viewport->setBackgroundColour(Ogre::ColourValue(0.0,0.0,0.0));
+
+    mCamera->setAspectRatio(
+        Ogre::Real(viewport->getActualWidth()) / Ogre::Real(viewport->getActualHeight())
+    );
+
+	// create plane
     Ogre::Plane plane(Ogre::Vector3::UNIT_Y, -5);
     Ogre::MeshManager::getSingleton().createPlane(
         "plane",
