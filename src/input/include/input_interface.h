9827ea3dc64194e6041d752683bbde5b0d3807d9
Serge Ratke <serge.ratke@gmx.de>
2011-09-03 12:03:10 +0200

    replaced the sdl key/modifier enum with own enums.
    created own enums for keys and modifiers to abstract the input
    interface, and make it possible to use other input libs. still
    a work in progress here.

--- src/input/include/input_interface.h
+++ src/input/include/input_interface.h
@@ -7,8 +7,23 @@
 #include <SDL/SDL.h>
 
 namespace Baukasten {
+	typedef enum {
+		KEY_NONE,
+		KEY_A, KEY_B, KEY_C, KEY_D, KEY_E, KEY_F, KEY_G,
+		KEY_H, KEY_I, KEY_J, KEY_K, KEY_L, KEY_M, KEY_N,
+		KEY_O, KEY_P, KEY_Q, KEY_R, KEY_S, KEY_T, KEY_U,
+		KEY_V, KEY_W, KEY_X, KEY_Y, KEY_Z
+	} Key;
+
+	typedef enum {
+		KEYMOD_NONE,
+		KEYMOD_LCTRL, KEYMOD_RCTRL, KEYMOD_CTRL,
+		KEYMOD_LSHIFT, KEYMOD_RSHIFT, KEYMOD_SHIFT,
+		KEYMOD_ALT
+	} Modifier;
+
 	typedef sigc::signal<void, SDL_Event*> InputEvent;
-	typedef sigc::signal<void, SDLKey, SDLMod> KeyDownEvent;
+	typedef sigc::signal<void, Key, Modifier> KeyDownEvent;
 
 	class BAUKASTEN_EXPORT InputInterface {
 	public:
