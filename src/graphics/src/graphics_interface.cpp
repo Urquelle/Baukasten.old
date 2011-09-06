f0d77a0874deccd014822902a2112c6e917a137a
Serge Ratke <serge.ratke@gmx.de>
2011-09-06 20:45:51 +0200

    don't set WITH_OGRE to TRUE as default.
    use irrlicht in case WITH_OGRE is not defined. the Ogre
    supporting cpp classes are not included/compiled when
    WITH_OGRE isn't defined.

--- src/graphics/src/graphics_interface.cpp
+++ src/graphics/src/graphics_interface.cpp
@@ -3,6 +3,8 @@
 
 #if defined(WITH_OGRE)
 #	include "ogre/ogre_interface.h"
+#else
+#	include "irrlicht/irrlicht_graphics.h"
 #endif
 
 using namespace Baukasten;
@@ -14,6 +16,8 @@ IGraphics* GraphicsInterface::instance()
 	if ( !mInstance ) {
 #if defined(WITH_OGRE)
 		mInstance = new OgreInterface();
+#else
+		mInstance = new IrrlichtGraphics();
 #endif
 	}
 
