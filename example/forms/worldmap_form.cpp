e0e1ff87527e1b7b2aadaa98a6130cfe50fd94a2
Serge Ratke <serge.ratke@gmx.de>
2011-09-07 20:41:04 +0200

    initialise and render the worldmap.

--- example/forms/worldmap_form.cpp
+++ example/forms/worldmap_form.cpp
@@ -2,14 +2,35 @@
 
 #include <irrlicht/irrlicht_graphics.h>
 
+#include <irrlicht/irrlicht.h>
+
 using namespace Baukasten;
 
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace io;
+using namespace gui;
+using namespace video;
+
 WorldMapForm::WorldMapForm( const std::string &id, IGraphics *graphics ) :
-	IrrlichtForm( id, dynamic_cast<IrrlichtGraphics*>( graphics ) )
+	IrrlichtForm( id, dynamic_cast<IrrlichtGraphics*>( graphics ) ),
+	mSceneManager( getGraphics()->getSceneManager() ),
+	mDriver( getGraphics()->getDriver() )
 {
+	mBackground = mDriver->getTexture("media/worldmap.jpg");
 }
 
 WorldMapForm::~WorldMapForm()
 {
+}
+
+void WorldMapForm::render()
+{
+	mDriver->draw2DImage(
+		mBackground, core::position2d<s32>(0,0),
+		core::rect<s32>(0,0,1024,768), 0,
+		video::SColor(255,255,255,255), true
+	);
 }
 
--- example/forms/worldmap_form.h
+++ example/forms/worldmap_form.h
@@ -7,10 +7,27 @@ namespace Baukasten {
 	class IGraphics;
 } /* Baukasten */
 
+namespace irr {
+	namespace scene {
+		class ISceneManager;
+	} /* scene */
+	namespace video {
+		class IVideoDriver;
+		class ITexture;
+	} /* video */
+} /* irr */
+
 class WorldMapForm : public Baukasten::IrrlichtForm {
 public:
 	WorldMapForm( const std::string&, Baukasten::IGraphics* );
 	virtual ~WorldMapForm();
+
+	void render();
+
+private:
+	irr::video::ITexture* 		mBackground;
+	irr::video::IVideoDriver*	mDriver;
+	irr::scene::ISceneManager*	mSceneManager;
 };
 
 #endif /* end of include guard: WORLDMAP_FORM_OPYB550X */
