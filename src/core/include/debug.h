bf72339e0d2275b1666cd930402cba5b27e9e5d1
Serge Ratke <serge.ratke@gmx.de>
2011-09-09 19:55:41 +0200

    add debug and assert macros.
    macros are active if NDEBUG isn't defined. debug  uses iostream's
    cout variable to print __FILE__, __LINE__ along with the given
    message to the standard output.
    
    assert macro exits the program if the test fails and prints the
    given message to the cerr.

--- src/core/include/debug.h
+++ src/core/include/debug.h
@@ -2,10 +2,22 @@
 #define DEBUG_QEAOHFVH
 
 #ifndef NDEBUG
-#	include <iostream>
-#	define BK_DEBUG( X ) std::cout << __FILE__ << " " << __LINE__ << ": " << X << std::endl;
+
+#include <iostream>
+#include <assert.h>
+
+#define BK_DEBUG( X ) std::cout << __FILE__ << " " << __LINE__ << ": " << X << std::endl;
+#define BK_ASSERT( TST, MSG )							\
+		( (TST) ? (void)0								\
+			: (std::cerr << __FILE__ "(" << __LINE__	\
+				<< "): Assertion failed " #TST			\
+				<< " " << MSG << std::endl,abort()))
+
 #else
-#	define BK_DEBUG( X )
+
+#define BK_DEBUG( X )
+#define BK_ASSERT( TST, MSG ) ( (void)0 )
+
 #endif
 
 #endif /* end of include guard: DEBUG_QEAOHFVH */
