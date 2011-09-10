2b33917ec6eec32ea7605607c79770ef635bf305
Serge Ratke <serge.ratke@gmx.de>
2011-09-10 15:01:19 +0200

    minor formatting changes.

--- src/core/src/state_manager.cpp
+++ src/core/src/state_manager.cpp
@@ -17,14 +17,12 @@ StateManager::~StateManager()
 void StateManager::addState( State *state )
 {
 	BK_ASSERT( state != 0, "state must not be 0." );
-
 	add( state );
 }
 
 void StateManager::addState( const std::string &id, State *state )
 {
 	BK_ASSERT( state != 0, "state must not be 0." );
-
 	add( id, state );
 }
 
