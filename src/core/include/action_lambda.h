704130a3189df400d02fea556ba299c43fad7df0
Serge Ratke <serge.ratke@gmx.de>
2011-09-13 18:25:02 +0200

    add actionlambda in order to be able to use lambdas.
    with this class it is possible to assign a lambda to done() and
    doAction() methods, thus avoid the writing of a new class.

---
+++ src/core/include/action_lambda.h
@@ -0,0 +1,28 @@
+#ifndef ACTION_LAMBDA_A41U943N
+#define ACTION_LAMBDA_A41U943N
+
+#include "global.h"
+
+#include "action.h"
+
+#include <algorithm>
+
+namespace Baukasten {
+	typedef std::function<bool ()> DoneFunction;
+	typedef std::function<void ( GameEntity* )> DoActionFunction;
+
+	class ActionLambda : public Action {
+	public:
+		ActionLambda( GameEntity&, const std::string&, DoneFunction*, DoActionFunction* );
+		virtual ~ActionLambda();
+
+		bool done() const;
+		void doAction( GameEntity* );
+
+	private:
+		DoneFunction*		mDone;
+		DoActionFunction*	mDoAction;
+	};
+} /* Baukasten */
+
+#endif /* end of include guard: ACTION_LAMBDA_A41U943N */
---
+++ src/core/src/action_lambda.cpp
@@ -0,0 +1,34 @@
+#include "action_lambda.h"
+
+#include "debug.h"
+
+using namespace Baukasten;
+
+ActionLambda::ActionLambda(
+		GameEntity &source,
+		const std::string &id,
+		DoneFunction *doneFunc,
+		DoActionFunction *doActFunc
+		) :
+	Action( source, id ),
+	mDone( doneFunc ),
+	mDoAction( doActFunc )
+{
+}
+
+ActionLambda::~ActionLambda()
+{
+}
+
+bool ActionLambda::done() const
+{
+	BK_ASSERT( mDone != 0, "function pointer must not be 0." );
+	return (*mDone)();
+}
+
+void ActionLambda::doAction( GameEntity *entity )
+{
+	BK_ASSERT( mDone != 0, "function pointer must not be 0." );
+	(*mDoAction)( entity );
+}
+
