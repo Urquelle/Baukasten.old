6bc1b33690c216cad222450cd365e23c98a14a04
Serge Ratke <serge.ratke@gmx.de>
2011-09-25 12:13:00 +0200

    add battle action to city entity.

--- example/entities/city.cpp
+++ example/entities/city.cpp
@@ -15,12 +15,17 @@ DoActionFunction runScene( []( Action *action, GameEntity *entity ) {
 	form->getState<StateInt*>( "state:mode" )->setValue( Mode::MODE_RUNSCENE );
 });
 
+DoActionFunction battle( []( Action *action, GameEntity *entity ) {
+	action->getSource()->getForm()->getState<StateInt*>( "state:mode" )->setValue( Mode::MODE_BATTLE );
+});
+
 City::City( const string &id, const string &filePath ) :
 	GameEntity( id )
 {
 	CoreServices *services = CoreServices::instance();
 
 	addAction( new ActionLambda( *this, "runScene", &runScene ) );
+	addAction( new ActionLambda( *this, "action:battle", &battle ) );
 
 	addState( new StateInt( "state:visited", 0 ) );
 	addState( new StateString( "state:name", "<unknown>" ) );
