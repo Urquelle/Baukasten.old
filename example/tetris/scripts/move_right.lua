b098f89218d6f1f04bb56e8e5f519db154337f7e
Serge Ratke <serge.ratke@gmx.de>
2011-09-27 22:23:08 +0200

    add min/max values for left/right movement.

--- example/tetris/scripts/move_right.lua
+++ example/tetris/scripts/move_right.lua
@@ -1,4 +1,7 @@
 block = source:getForm():getVSpace():getEntity( "block:current" )
 current_position = block:getPosition()
 next_position = t_pos( current_position:getX() + 40, current_position:getY(), current_position:getZ() )
-block:setPosition( next_position )
+
+if next_position:getX() < 640 then
+	block:setPosition( next_position )
+end
