0ab98855d0d70460872a34315910ebf9b9a71ed8
Serge Ratke <serge.ratke@gmx.de>
2011-10-04 21:08:42 +0200

    draw current block at the top of the field.

--- example/tetris/field_form.cpp
+++ example/tetris/field_form.cpp
@@ -9,6 +9,9 @@
 
 using namespace Baukasten;
 
+const int FIELD_WIDTH = 13;
+const int BLOCK_WIDTH = 4;
+
 FieldForm::FieldForm( const string &id, IGraphics *graphics ) :
 	IrrlichtForm( id, dynamic_cast<IrrlichtGraphics*>( graphics ) )
 {
@@ -32,8 +35,9 @@ void FieldForm::render()
 	t_size size = getSize();
 
 	irr::video::SColor cSet( 255, 0, 0, 0 );
+	irr::video::SColor cBlock( 255, 125, 125, 0 );
 
-	for_each( matrix.begin(), matrix.end(), [&i, &j, &pos, &size, driver, this, &cSet]( int k ) {
+	for_each( matrix.begin(), matrix.end(), [&i, &j, &pos, &row, &column, &block, driver, this, &cSet, &cBlock]( int k ) {
 		int x = pos.getX() + 40 * ( i % 13 );
 		int y = pos.getY() + 40 * j;
 
@@ -42,6 +46,19 @@ void FieldForm::render()
 			driver->draw2DRectangle(
 				cSet, irr::core::rect<s32>( x, y, x + 40, y + 40 )
 			);
+		}
+
+		// draw current block
+		int startBlock = ( row * FIELD_WIDTH ) + ( column - 1 );
+		int endBlock = ( ( row + 3 ) * FIELD_WIDTH ) + ( column + 2 );
+		int range = ( i - column + 1 ) % FIELD_WIDTH;
+		if ( block.size() && i >= startBlock && i <= endBlock && range >= 0 && range < BLOCK_WIDTH ) {
+			int index = ( i - column + 1 ) - ( FIELD_WIDTH - BLOCK_WIDTH ) * ( j - row );
+			if ( block[ index ] ) {
+				driver->draw2DRectangle(
+					cBlock, irr::core::rect<s32>( x, y, x + 40, y + 40 )
+				);
+			}
 		}
 
 		++i;
