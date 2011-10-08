199678e29961e814325c91e16e23ee957534b248
Serge Ratke <serge.ratke@gmx.de>
2011-10-08 18:02:10 +0200

    operate upon the new block:column state in the field's form.

--- example/tetris/scripts/rotate.lua
+++ example/tetris/scripts/rotate.lua
@@ -1,7 +1,7 @@
 block = source:getForm():getVSpace():getEntity( "block:current" )
 field = source:getChild("entity:field")
 
-column = field:getState("state:column"):getValue()
+column = field:getForm():getState("block:column"):getValue()
 
 curr_matrix = block:getState( "state:currentMatrix" )
 next_matrix_num = ( curr_matrix:getValue() % 4 ) + 1
@@ -15,7 +15,7 @@ limit_left = curr_limits:getValue( 3 )
 block_min = column - limit_left
 block_max = column + limit_right
 
-if block_min >= 0 and block_max <= 13 then
+if block_min >= 0 and block_max <= 12 then
 	curr_matrix:setValue( next_matrix_num )
 	matrix:setValues(block:getState( "state:matrix" .. next_matrix_num ))
 	field:getForm():getState("block:current"):setValues(matrix)
