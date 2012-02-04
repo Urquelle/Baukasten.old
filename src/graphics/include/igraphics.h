ab5fee83b82cd86690c3683af9e5391a354e05aa
Serge Ratke <serge.ratke@gmx.de>
2012-02-04 10:57:24 +0100

    core.graphics: introduce a new method to draw circles.
    this method simply draws the circles at the given position
    with the given radius.

--- src/graphics/include/igraphics.h
+++ src/graphics/include/igraphics.h
@@ -18,6 +18,7 @@ namespace Baukasten {
 
 		virtual void render( Form* ) = 0;
 
+		virtual void drawCircle( const v3<float>&, const uint, const Colour& ) = 0;
 		virtual void drawImage( const string&, const v2<float>&, const v3<float>& ) = 0;
 		virtual void drawLine( const v3<float>&, const v3<float>&, const Colour& ) = 0;
 		virtual void drawPoint( const v3<float>&, const uint, const Colour& ) = 0;
