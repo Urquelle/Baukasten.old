6430078c16c6bb43bfc1fef9a9a4f432925eed1e
Serge Ratke <serge.ratke@gmx.de>
2012-04-01 18:42:38 +0200

    unittest: add unit tests for new vector operations.

--- test/math_test.h
+++ test/math_test.h
@@ -4,6 +4,8 @@
 #include "model/Global"
 #include "core/Math"
 
+#include <cmath>
+
 using namespace Baukasten;
 using namespace CppUnit;
 
@@ -19,6 +21,15 @@ class MathTest : public TestFixture {
 	CPPUNIT_TEST( testAdditionVec3 );
 	CPPUNIT_TEST( testEqualVec3 );
 	CPPUNIT_TEST( testNotEqualVec3 );
+	CPPUNIT_TEST( testPlusValueVec3 );
+	CPPUNIT_TEST( testPlusVecVec3 );
+	CPPUNIT_TEST( testMultVecVec3 );
+	CPPUNIT_TEST( testMultValueVec3 );
+	CPPUNIT_TEST( testMagnitudeVec3 );
+	CPPUNIT_TEST( testMagnitudeSqrVec3 );
+	CPPUNIT_TEST( testNormVec3 );
+	CPPUNIT_TEST( testDotVec3 );
+	CPPUNIT_TEST( testCrossVec3 );
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void setUp()
@@ -137,6 +148,116 @@ public:
 		vec3<float> v2( 3.0, 5.0, 0.0 );
 
 		CPPUNIT_ASSERT( v1 != v2 );
+	}
+
+	void testPlusValueVec3()
+	{
+		float value = 2.0;
+		vec3f v1{ 1.0, 2.0, 3.5 };
+
+		v1 = v1 + value;
+
+		CPPUNIT_ASSERT( v1[BK_X] == 3.0 );
+		CPPUNIT_ASSERT( v1[BK_Y] == 4.0 );
+		CPPUNIT_ASSERT( v1[BK_Z] == 5.5 );
+	}
+
+	void testPlusVecVec3()
+	{
+		vec3f v1{ 1.0, 2.0, 3.5 };
+		vec3f v2{ 2.5, 10.0, 5.7 };
+
+		v1 = v1 + v2;
+
+		f32 eX = 3.5;
+		f32 eY = 12.0;
+		f32 eZ = 9.2;
+		f32 d = 0.00000000000001;
+
+		CPPUNIT_ASSERT( fabs(v1[BK_X] - eX ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Y] - eY ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Z] - eZ ) < d );
+	}
+
+	void testMultVecVec3()
+	{
+		vec3f v1{ 2.0, 2.0, 3.5 };
+		vec3f v2{ 2.5, 10.0, 4.0 };
+
+		v1 = v1 * v2;
+
+		f32 eX =  5.0;
+		f32 eY = 20.000000000000001;
+		f32 eZ = 14.0;
+		f32 d  =  0.00000000000001;
+
+		CPPUNIT_ASSERT( fabs(v1[BK_X] - eX ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Y] - eY ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Z] - eZ ) < d );
+	}
+
+	void testMultValueVec3()
+	{
+		vec3f v1{ 2.0, 7.0, 3.5 };
+		f32 value = 4.0;
+
+		v1 = v1 * value;
+
+		f32 eX =  8.0;
+		f32 eY = 28.000000000000001;
+		f32 eZ = 14.0;
+		f32 d  =  0.00000000000001;
+
+		CPPUNIT_ASSERT( fabs(v1[BK_X] - eX ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Y] - eY ) < d );
+		CPPUNIT_ASSERT( fabs(v1[BK_Z] - eZ ) < d );
+	}
+
+	void testMagnitudeVec3()
+	{
+		vec3f v1{ 2.0, 7.0, 3.5 };
+		f32 e = sqrt( 4.0 + 49.0 + 12.25 ); // sqrt( 65.25 ) == 8.0777472..
+		f32 d  =  0.00000000000001;
+
+		CPPUNIT_ASSERT( fabs( v1.mag() - e ) < d );
+	}
+
+	void testMagnitudeSqrVec3()
+	{
+		vec3f v1{ 2.0, 7.0, 3.5 };
+		f32 e = 4.0 + 49.0 + 12.25; // 65.25
+		f32 d  =  0.00000000000001;
+
+		CPPUNIT_ASSERT( fabs( v1.magSqr() - e ) < d );
+	}
+
+	void testNormVec3()
+	{
+		vec3f v1{ 1.0, 1.0, 1.0 };
+		f32 mag = v1.norm().mag();
+		f32 ex = 1.0;
+		f32 d  =  0.0000001;
+
+		CPPUNIT_ASSERT( fabs( v1.norm().mag() - ex ) <= d );
+	}
+
+	void testDotVec3()
+	{
+		vec3f v1{ 2.0, 3.0, 4.0 };
+		f32 e = 4.0 + 9.0 + 16.0;
+		f32 d  =  0.00000000000001;
+
+		CPPUNIT_ASSERT( v1.dot( v1 ) == e );
+	}
+
+	void testCrossVec3()
+	{
+		vec3f v1{ 1.0, 1.0, 0.0 };
+		vec3f v2{ 1.0, 0.0, 1.0 };
+		vec3f v3 = v1.cross( v2 );
+		vec3f ex{ 1.0, -1.0, -1.0 };
+
+		CPPUNIT_ASSERT( v3 == ex );
 	}
 
 private:
