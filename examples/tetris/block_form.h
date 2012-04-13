fa5d3543288968858f65c8e94107340cff2b5942
Serge Ratke <serge.ratke@gmx.de>
2012-04-13 22:57:47 +0200

    examples.tetris: move block form code to the header.

--- examples/tetris/block_form.h
+++ examples/tetris/block_form.h
@@ -1,17 +1,54 @@
 #ifndef BLOCK_FORM_21IENA7S
 #define BLOCK_FORM_21IENA7S
 
+#include "core/Color"
+#include "graphics/IGraphics"
 #include "model/Form"
+#include "model/GenericState"
 
-class BlockForm : public Baukasten::Form {
+#include "constants.h"
+
+using namespace Baukasten;
+
+class BlockForm : public Form {
 public:
-	const int BLOCK_WIDTH;
-	const int BLOCK_HEIGHT;
+	BlockForm( const std::string &id ) :
+        Form( id )
+    {
+    }
 
-	BlockForm( const std::string& );
-	virtual ~BlockForm();
+	virtual ~BlockForm()
+    {
+    }
 
-	void render();
+	virtual void render()
+    {
+        auto matrix = state<StateIntVector*>( "state:matrix1" )->values();
+
+        Color cSet( Color::BK_RED );
+        Color cClean( Color::BK_WHITE );
+
+        int x = position()[BK_X];
+        int x_offset = 0;
+        int y = position()[BK_Y];
+        int y_offset = 0;
+        vec3<float> pos;
+        vec2<float> size({ (float)(BLOCK_PX_WIDTH), (float)(BLOCK_PX_HEIGHT) });
+
+        for ( int i = 0; i < BLOCK_SIZE; ++i ) {
+            x_offset = ( i % BLOCK_WIDTH ) * BLOCK_PX_WIDTH;
+            y_offset = (int)( i / BLOCK_HEIGHT ) * BLOCK_PX_HEIGHT;
+
+            pos.set( BK_X, (float)(x + x_offset) );
+            pos.set( BK_Y, (float)(y + y_offset) );
+
+            if ( matrix[ i ] == SET ) {
+                graphics().drawRect( size, pos, cSet );
+            } else if ( matrix[ i ] == CLEAN ) {
+                graphics().drawRect( size, pos, cClean );
+            }
+        }
+    }
 };
 
 #endif /* end of include guard: BLOCK_FORM_21IENA7S */
